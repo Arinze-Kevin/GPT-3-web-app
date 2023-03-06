665d14610fc9bd51f4bcbdf0450ddceff7c32058
Arinze-Kevin <egboguarinze@gmail.com>
2023-03-06 07:23:14 +0100

    Made final changes

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,47 +1,19 @@
-import React from 'react';
-import './footer.css';
-import gpt3Logo from '../../assets/logo.svg';
-
-const Footer = () => {
-  return (
-    <div className='gpt3__footer section__padding'>
-      <div className='gpt3__footer-heading'>
-        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
-      </div>
-      <div className='gpt3__footer-btn'>
-        <p>Request Early Access</p>
-      </div>
-      <div className='gpt3__footer-links'>
-        <div className='gpt3__footer-links_logo'>
-          <img src={gpt3Logo} alt="logo" />
-          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
-        </div>
-        <div className='gpt3__footer-links_div'>
-          <h4>Links</h4>
-          <p>Overons</p>
-          <p>Social Media</p>
-          <p>Counters</p>
-          <p>Contact</p>
-        </div>
-        <div className='gpt3__footer-links_div'>
-          <h4>Company</h4>
-          <p>Terms & Conditions</p>
-          <p>Social Media</p>
-          <p>Privacy Policy</p>
-          <p>Contact</p>
-        </div>
-        <div className='gpt3__footer-links_div'>
-          <h4>Get in touch</h4>
-          <p>Crechterwoord K12 182 DK Alknjkcb</p>
-          <p>+012-1234567</p>
-          <p>iam.arinzekevin@gmail.com</p>
-        </div>
-      </div>
-      <div className='gpt3__footer-copyright'>
-        <p>© 2021 GPT-3. All rights reserved.</p>
-      </div> 
-    </div>
-  )
-}
-
-export default Footer
+import { useState } from "react"; 
+ 
+export default function App() { 
+  const [greeting, setGreeting] = useState({ greet: "Hello, World" }); 
+  console.log(greeting, setGreeting); 
+ 
+  function updateGreeting() { 
+    const newGreeting = {...greeting}; 
+    newGreeting.greet = "Hello, World-Wide Web"; 
+    setGreeting(newGreeting); 
+  } 
+ 
+  return ( 
+    <div> 
+      <h1>{greeting.greet}</h1> 
+      <button onClick={updateGreeting}>Update greeting</button> 
+    </div> 
+  ); 
+} 
